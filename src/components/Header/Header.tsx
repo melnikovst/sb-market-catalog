5a5d4ad416f1f390a10cc19ce89bb136a42ce226
Stanislav Melnikov <melnikov.stan1slav@yandex.ru>
2022-10-17 19:52:03 +0300

    header done

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+import logo from '../../images/headerLogo.svg';
+import styles from './Header.module.scss'
+
+
+const Header = () => {
+
+    console.log(styles.wrapper)
+    return (
+        <header className={styles.header}>
+            <div className={styles.wrapper}>
+                <Link to={'/'}>
+                    <img src={logo} alt="header-logo" />
+                </Link>
+                <nav className={styles.links}>
+                    <Link to={'/'} className={styles.link}>женщины</Link>
+                    <Link to={'/'} className={styles.link}>мужчины</Link>
+                    <Link to={'/'} className={styles.link}>мы</Link>
+                </nav>
+                <Link to={'/cart'} className={styles.cart} />
+            </div>
+        </header>
+    )
+}
+
+export default Header;
